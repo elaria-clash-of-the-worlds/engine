4a90d868fe1fa57ff96ee05a657524cd02b662ef
Maxim <maze.zte@gmail.com>
2024-01-12 14:04:02 +1000

    Add some stuff in demo

--- elaria/examples/ExampleLevel1.js
+++ elaria/examples/ExampleLevel1.js
@@ -12,7 +12,9 @@ class ExampleLevel1 extends SceneDescription {
 
     build() {
         const player = this.createGameObject("Player");
-        player.addComponent(Player);
+        player.transform.position = new Vector2D(100, 100);
+        const playerComponent = player.addComponent(Player);
+        playerComponent.loadSceneIndex = 1;
 
         const sprite = player.addComponent(Sprite);
         sprite.imageSource = "https://mosnapitki.ru/images/cms/thumbs/8c856c34813e8e6eaa248115eef774bea5ffd7a1/sprite_lemon_lime_330ml__535_535_1_100.jpg"

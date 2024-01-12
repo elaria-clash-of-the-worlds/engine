8eb9b7ee024d95b3ebfbdcecab8aff8a915d37a9
Anatoly Frolov <contact@anafro.ru>
2024-01-12 11:38:25 +1000

    Added sprite resizing support

--- elaria/core/Sprite.js
+++ elaria/core/Sprite.js
@@ -1,13 +1,9 @@
 import Component from "./Component.js";
 import ElariaGame from "./ElariaGame.js";
 
-export default class Sprite extends Component{
+export default class Sprite extends Component {
     #ctx;
     #image = new Image();
-
-    get imageSource() {
-        return this.#image.src;
-    }
 
     set imageSource(imageSource) {
         this.#image.src = imageSource;
@@ -17,12 +13,20 @@ export default class Sprite extends Component{
         return this.#image;
     }
 
+    set width(newWidth) {
+        this.#image.width = newWidth;
+    }
+
+    set height(newHeight) {
+        this.#image.height = newHeight;
+    }
+
     awake() {
         this.#ctx = ElariaGame.canvas.getContext("2d");
     }
 
     render() {
         this.#ctx.fillStyle = "green";
-        this.#ctx.drawImage(this.#image, this.transform.position.x, this.transform.position.y);
+        this.#ctx.drawImage(this.#image, this.transform.position.x, this.transform.position.y, this.#image.width, this.#image.height);
     }
 }

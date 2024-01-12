809a0e2a44a15171e3f756cdde0d3b087d172f7a
Anatoly Frolov <contact@anafro.ru>
2024-01-12 14:06:13 +1000

    Made acceleration slowing down

--- elaria/examples/Player.js
+++ elaria/examples/Player.js
@@ -19,33 +19,31 @@ export default class Player extends Component {
     }
 
     update(dt) {
-        kd.SHIFT.down(() => {
-            this.acceleration += this.acceleration * dt;
-        });
+        this.acceleration += (kd.SHIFT.isDown() ? 1 : -1) * this.acceleration * dt;
 
-        kd.W.down(() => {
+        if (kd.W.isDown()) {
             if (this.velocity.length <= Player.MAX_SPEED)
                 this.velocity.y -= this.acceleration;
-        });
+        }
 
-        kd.A.down(() => {
+        if (kd.A.isDown()) {
             if (this.velocity.length <= Player.MAX_SPEED)
                 this.velocity.x -= this.acceleration;
-        });
+        }
 
-        kd.S.down(() => {
+        if (kd.S.isDown()) {
             if (this.velocity.length <= Player.MAX_SPEED)
                 this.velocity.y += this.acceleration;
-        });
+        }
 
-        kd.D.down(() => {
+        if (kd.D.isDown()) {
             if (this.velocity.length <= Player.MAX_SPEED)
                 this.velocity.x += this.acceleration;
-        });
+        }
 
-        kd.SPACE.down(() => {
+        if (kd.SPACE.isDown()) {
             this.gameObject.getComponent(AudioSource).play();
-        });
+        }
 
         this.transform.position = this.transform.position.add(new Vector2D(this.velocity.x * dt, this.velocity.y * dt));
 

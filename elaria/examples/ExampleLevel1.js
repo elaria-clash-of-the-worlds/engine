0eab4c41f8e96210dedaa922380f5016596bb4c3
Anatoly Frolov <contact@anafro.ru>
2024-01-12 11:59:45 +1000

    Added an audio source

--- elaria/examples/ExampleLevel1.js
+++ elaria/examples/ExampleLevel1.js
@@ -1,6 +1,7 @@
 import SceneDescription from "../core/SceneDescription.js";
 import Player from "./Player.js";
 import Sprite from "../core/Sprite.js";
+import AudioSource from "../core/AudioSource.js";
 
 class ExampleLevel1 extends SceneDescription {
     constructor() {
@@ -14,6 +15,8 @@ class ExampleLevel1 extends SceneDescription {
         sprite.imageSource = "https://mosnapitki.ru/images/cms/thumbs/8c856c34813e8e6eaa248115eef774bea5ffd7a1/sprite_lemon_lime_330ml__535_535_1_100.jpg"
         sprite.width = 100;
         sprite.height = 100;
+        const audioSource = player.addComponent(AudioSource);
+        audioSource.source = '/elaria/examples/assets/bubbles.wav';
     }
 }
 

79a669519c12daaa921e64ccd2e8143f2016cc37
Maxim <maze.zte@gmail.com>
2024-02-27 15:45:58 +1000

    Add reexport of core components

---
+++ elaria/core/ElariaCore.js
@@ -0,0 +1,9 @@
+export {default as ElariaGame} from "./ElariaGame.js";
+export {default as GameObject} from "./GameObject.js";
+export {default as Component} from "./Component.js";
+export {default as Scene} from "./Scene.js";
+export {default as SceneManager} from "./SceneManager.js";
+export {default as SceneDescription} from "../core/SceneDescription.js";
+export {default as Sprite} from "../core/Sprite.js";
+export {default as AudioSource} from "../core/AudioSource.js";
+export {default as Vector2D} from "../core/Vector2D.js";
